18ef0a4b2f190345078f071a02c31da31c5cdffe
Eclip5e <32602757+Eclip5eLP@users.noreply.github.com>
2021-10-04 16:37:01 +0200

    Fishing fix

--- Fishing Bot/core.py
+++ Fishing Bot/core.py
@@ -7,7 +7,7 @@ import sys
 import subprocess
 import pkg_resources
 
-required = {'pyautogui', 'keyboard', 'colorama', 'datetime', 'pywin32', 'opencv-python'}
+required = {'pyautogui', 'keyboard', 'colorama', 'datetime', 'pywin32', 'opencv-python', 'pydirectinput'}
 installed = {pkg.key for pkg in pkg_resources.working_set}
 missing = required - installed
 
@@ -21,6 +21,7 @@ from pyautogui import *
 from datetime import datetime, timedelta
 from colorama import init, Fore, Back, Style
 import pyautogui
+import pydirectinput
 import time
 import keyboard
 import random
@@ -36,8 +37,7 @@ with open('settings.json') as f:
 appName = "New World Fishing Bot"
 bait = settings["bait"]
 reelTime = settings["reelTime"]
-reposX = settings["reposX"]
-reposY = settings["reposY"]
+repairCycle = settings["repairCycle"]
 
 #Vars
 version = "0.1"
@@ -68,10 +68,36 @@ def type(msg):
 		pyautogui.press(x)
 	return True
 
+#Repair Fishing Rod
+def repair():
+	stateMsg = "Repairing fishing rod..."
+	pydirectinput.keyUp('altleft')
+	time.sleep(0.5)
+	pydirectinput.press('tab')
+	time.sleep(0.5)
+	find = search('rodf3', 0.7, True)
+	if find != False:
+		pydirectinput.keyDown('r')
+		time.sleep(0.5)
+		click(find.left - 50, find.top + 5)
+		time.sleep(0.5)
+		pydirectinput.keyUp('r')
+		time.sleep(0.1)
+		pydirectinput.press('e')
+		time.sleep(0.5)
+		pydirectinput.press('tab')
+		time.sleep(0.5)
+		pydirectinput.press('f3')
+		time.sleep(0.5)
+		return True
+	else:
+		return False
+
 time.sleep(2)
 print(Fore.GREEN + "Bot running!" + Fore.WHITE)
 
 state = 1
+_repairCycle = 0
 pyautogui.FAILSAFE = False
 stateMsg = "Checking for active fishing..."
 print("Checking...")
@@ -82,8 +108,13 @@ while True:
 	find = search('tab', 0.8)
 	if find != False:
 		#Fishing Loop
+		if _repairCycle == repairCycle:
+			_repairCycle = 0
+			repair()
+
 		if state == 1: #Check Fishing
 			stateMsg = "Checking for active fishing..."
+			pydirectinput.keyUp('altleft')
 			find = search('hook', 0.8)
 			if find != False:
 				state = 2
@@ -100,7 +131,7 @@ while True:
 			find = search('catch', 0.7, True)
 			if find != False:
 				pyautogui.click(button='left')
-				region = pyautogui.screenshot('./images/region.png', region=(100, 100, 150, 150))
+				pydirectinput.keyDown('altleft')
 				state = 4
 			else:
 				#Check Completed
@@ -125,19 +156,8 @@ while True:
 			#Check Completed
 			find = search('hook', 0.7, True)
 			if find != False:
-				state = 5
-		elif state == 5: #Check correct angle
-			stateMsg = "Checking angle..."
-			find = search('region', 0.75, False)
-			time.sleep(0.5)
-			if find != False:
 				state = 1
-			else:
-				#Change View
-				stateMsg = "Fixing angle..."
-				win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, reposX, reposY, 0, 0)
-				time.sleep(1)
-				state = 1
+				_repairCycle += 1
 
 		#State Output
 		sys.stdout.write('\x1b[1A')
